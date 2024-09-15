97dba5bad3363c0a4ffeec520b48c07eeab2f08b
layla.khdva <layla.khdva@gmail.com>
2024-09-15 13:56:53 +0000

    first commit

---
+++ lib/tab/smoothie_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SmoothieTab extends StatelessWidget {
+  const SmoothieTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
+    return Container(
+      child: const Center(
+        child: Text("SMOOTHIE TAB"),
+      ),
+    );
+  }
+}
